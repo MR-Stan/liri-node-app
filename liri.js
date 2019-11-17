ee8d284be16ce5cd8b702dd54f8fe8ec2eff4fa6
MR-Stan <michaelrstanley87@gmail.com>
2019-11-17 10:12:26 -0600

    added song info function

--- liri.js
+++ liri.js
@@ -4,6 +4,8 @@ require("dotenv").config();
 
 let keys = require("./keys.js");
 
+let Spotify = require('node-spotify-api');
+
 let spotify = new Spotify(keys.spotify);
 
 let axios = require("axios");
@@ -15,21 +17,21 @@ let action = process.argv[2];
 // switch statement determines which function to run
 switch (action) {
     case "concert-this":
-        concertInfo();
+        getConcertInfo();
         break;
     case "spotify-this-song":
-        songInfo();
+        getSongInfo();
         break;
     case "movie-this":
-        movieInfo();
+        getMovieInfo();
         break;
     case "do-what-it-says":
-        randomInfo();
+        getRandomInfo();
         break;
 }
 
 // function for retrieving concert info using band name
-function concertInfo() {
+function getConcertInfo() {
     // input artist name converted to string with no spaces
     let artistName = process.argv.splice(3).join().replace(/,/g, "");
 
@@ -48,11 +50,46 @@ function concertInfo() {
         }
     });
 }
-
+// NOT RETURNING RESULTS
 // function for song info
+function getSongInfo() {
+    // input song name converted to string with no spaces
+    let songName = process.argv.splice(3).join().replace(/,/g, "");
+
+    if (songName.length === 0) {
+        console.log("fail")
+    }
+    else {
+        spotify.search({
+            type: "track",
+            query: songName,
+            limit: 5
+        }, function (err, data) {
+            if (err) {
+                fs.appendFileSync("log.txt", "Error: " + err, "utf8");
+                return console.log("Error: " + err);
+            }
+
+            for (let i = 0; i < data.tracks.items.length; i++) {
+                songInfo = ["\nSong Title: " + data.tracks.items[i].name +
+                    ", Album Title: " + data.tracks.items[i].album.name +
+                    ", Artist(s) Name: " + data.tracks.items[i].artists[0].name +
+                    ", Preview URL: " + data.tracks.items[i].preview_url + '\n'].join('\n');
+                console.log(songInfo);
+                fs.appendFileSync("log.txt", songInfo, function (err) {
+                    if (err) throw err;
+                });
+            }
+        });
+    }
+}
 
 // function for movie info
+function getMovieInfo() {
+
+}
 
 // function to read and execute .txt file
+function getRandomInfo() {
 
-concertInfo();
+}
